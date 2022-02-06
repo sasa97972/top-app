ca77ad5944946c6ce8ba4e50082844904782a2a6
Oleksandr Dulia <odul@ciklum.com>
2022-02-06 14:20:34 +0200

    Clear the project, add Custom Document component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import { AppProps } from "next/dist/shared/lib/router/router";
-import "../styles/globals.scss";
+import { AppProps } from "next/app";
+import Head from "next/head";
+import  "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Top Application</title>
+                <meta name="description" content="Some description for app" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
