cd235a406b0c0fb25ae10961d59055a5dbf00de3
Oleksandr Dulia <odul@ciklum.com>
2022-02-07 13:12:57 +0200

    Add default font and colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-import  "../styles/globals.scss";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Head>
-                <title>Top Application</title>
-                <meta name="description" content="Some description for app" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+	return (
+		<>
+			<Head>
+				<title>Top Application</title>
+				<meta name="description" content="Some description for app" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,11 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="ru">
-                <Head />
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
