8e0309d23f35d0f5cc9716a3aff271ba7403ea39
Oleksandr Dulia <odul@ciklum.com>
2022-03-08 11:59:08 +0200

    Add styles for layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -2,18 +2,17 @@ import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { LayoutProps } from "./Layout.props";
 import { Sidebar } from "./Sidebar";
+import styles from "./Layout.module.scss";
 
 export const Layout = ({ children }: LayoutProps) => {
 	return (
-		<>
-			<Header/>
-			<div>
-				<Sidebar/>
-				<main>
-					{ children }
-				</main>
-			</div>
-			<Footer/>
-		</>
+		<div className={styles.wrapper}>
+			<Header className={styles.header} />
+			<Sidebar className={styles.sidebar} />
+			<main className={styles.body}>
+				{ children }
+			</main>
+			<Footer className={styles.footer} />
+		</div>
 	);
 };
