7f095203ace40a08ee97676c406fa71f4ce0fa6b
Oleksandr Dulia <odul@ciklum.com>
2022-03-22 20:35:00 +0200

    Add logo

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,4 +1,6 @@
 import classNames from "classnames";
+import Link from "next/link";
+import Logo from "../images/logo.svg";
 import { Menu } from "../Menu";
 import styles from "./Sidebar.module.scss";
 import { SidebarProps } from "./Sidebar.props";
@@ -6,6 +8,11 @@ import { SidebarProps } from "./Sidebar.props";
 export function Sidebar({ className, ...props }: SidebarProps) {
     return (
         <aside className={classNames(styles.sidebar, className)} {...props}>
+            <Link href="/">
+                <a className={styles.logoLink}>
+                    <Logo className={styles.logo} />
+                </a>
+            </Link>
             <Menu />
         </aside>
     );
