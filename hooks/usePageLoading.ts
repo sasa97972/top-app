7849a485a6a3668f40aaf6a7bce618f00b99487f
Oleksandr Dulia <odul@ciklum.com>
2022-04-24 21:59:16 +0300

    Update packages (React 18), add nprogress

---
+++ hooks/usePageLoading.ts
@@ -0,0 +1,26 @@
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
+import { useEffect } from "react";
+
+export const usePageLoading = (): void => {
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleStart = (url: string): void => {
+            NProgress.start();
+        };
+        const handleStop = (): void => {
+            NProgress.done();
+        };
+
+        router.events.on("routeChangeStart", handleStart);
+        router.events.on("routeChangeComplete", handleStop);
+        router.events.on("routeChangeError", handleStop);
+
+        return () => {
+            router.events.off("routeChangeStart", handleStart);
+            router.events.off("routeChangeComplete", handleStop);
+            router.events.off("routeChangeError", handleStop);
+        };
+    }, [router]);
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import Head from "next/head";
+import { usePageLoading } from "../hooks/usePageLoading";
 import { AppPropsWithLayout } from "../types/nextapp";
 
 import "../styles/globals.scss";
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
     const getLayout = Component.getLayout ?? ((page) => page);
+
+    usePageLoading();
 
     return (
         <>
